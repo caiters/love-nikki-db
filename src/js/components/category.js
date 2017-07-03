badd71078943940527cdf14d76b03caaa15b0d29
Caitlin <ayashi@valefor.com>
2017-07-02 21:38:27 -0700

    adding categories!

--- src/js/components/category.js
+++ src/js/components/category.js
@@ -1,21 +1,6 @@
 Vue.component('category', {
   template: `
-  <div class="form-group">
-    <label class="form-group__label" for="category">Category</label>
-    <select id="category">
-      <option v-for="category in categories">{{category.name}}</option>
-    </select>
-  </div>
+  <option :value="item.name.replace(/&nbsp;/gi,'')" v-html="item.name" :disabled="item.type === 'container'"></option>
   `,
-  props: ['categories'],
-  data: function(){
-    return {
-      selectedTags: []
-    }
-  },
-  methods: {
-    addedTag: function(){
-      this.$emit('change', this.selectedTags);
-    }
-  }
+  props: ['item']
 });
